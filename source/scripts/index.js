ba67f2e7f900ab3a6b667a347c922377b949b126
Termi1878 <termi1878@MacBook-Pro-Termi1878.local>
2019-06-13 18:57:17 +0200

    some changes in html and javascript

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,22 +1,39 @@
 const ico = document.querySelector('.burger');
-        ico.addEventListener('click', function () {
-            this.classList.toggle('active');
-            document.querySelector('.menu').classList.toggle('none__menu');
-            document.querySelector('.navbar').classList.toggle('display__none');
-            document.querySelector('.burger--home').classList.toggle('burger--white');
-            document.querySelector('.logo svg').classList.toggle('logo__invert');
-        });
-        var basketCount = document.querySelectorAll('.basket__item').length;
-        document.querySelector('.basket__count span').innerText += basketCount;
-        document.querySelector
-        var priceCheckout = document.querySelector('.price__result');
-        var itemsCalc = document.querySelectorAll('.basket__content .price__calc');
-        var count = 0;
-        for (let i = 0; i < itemsCalc.length; i++) {
-            count += parseInt(itemsCalc[i].innerText);
-        }
-        priceCheckout.innerText = count.toString();
-        var itemsPrice = document.querySelectorAll('.basket .price__calc');
-        for (let i = 0; i < itemsPrice.length; i++) {
-            itemsPrice[i].innerText += ' zł';
-        }
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+const burgerHome = document.querySelector('.burger--home');
+const logo = document.querySelector('.logo svg');
+ico.addEventListener('click', () => {
+    ico.classList.toggle('active');
+    if (menu) {
+        menu.classList.toggle('none__menu');
+    }
+    if (navbar) {
+        navbar.classList.toggle('display__none');
+    }
+    if (burgerHome) {
+        burgerHome.classList.toggle('burger--white')
+    };
+    if (logo) {
+        logo.classList.toggle('logo__invert');
+    }
+});
+const basketCount = document.querySelectorAll('.basket__item').length;
+const basket = document.querySelector('.basket__count span');
+if (basket) {
+    basket.innerText += basketCount;
+}
+const priceCheckout = document.querySelector('.price__result');
+const itemsCalc = document.querySelectorAll('.basket__content .price__calc');
+let count = 0;
+itemsCalc.forEach(e => {
+    count += parseInt(e.innerText);
+});
+if (priceCheckout) {
+    priceCheckout.innerText = count.toString();
+}
+var itemsPrice = document.querySelectorAll('.basket .price__calc');
+
+itemsPrice.forEach(e => {
+    e.innerText += ' zł';
+});
