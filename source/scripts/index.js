0abc37e5b96c3214556933c80b1a9d96378a2df2
Termi1878 <termi1878@MacBook-Pro-Termi1878.local>
2019-06-14 15:43:27 +0200

    added listener on wishlist button and add to cart

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,21 @@ const menu = document.querySelector('.menu');
 const navbar = document.querySelector('.navbar');
 const burgerHome = document.querySelector('.burger--home');
 const logo = document.querySelector('.logo svg');
+const basketCount = document.querySelectorAll('.basket__item').length;
+const basket = document.querySelector('.basket__count span');
+const priceCheckout = document.querySelector('.price__result');
+const itemsCalc = document.querySelectorAll('.basket__content .price__calc');
+const itemsPrice = document.querySelectorAll('.basket .price__calc');
+const wishList = document.querySelector('.wishlist');
+if(wishList){
+    wishList.addEventListener('click', () =>{
+        const wishRed = document.querySelectorAll('.st0');
+        wishList.classList.toggle('.wishBig')
+        wishRed.forEach(e =>{
+            e.classList.toggle('wishred');
+        })
+    });
+}
 ico.addEventListener('click', () => {
     ico.classList.toggle('active');
     if (menu) {
@@ -18,13 +33,10 @@ ico.addEventListener('click', () => {
         logo.classList.toggle('logo__invert');
     }
 });
-const basketCount = document.querySelectorAll('.basket__item').length;
-const basket = document.querySelector('.basket__count span');
+
 if (basket) {
     basket.innerText += basketCount;
 }
-const priceCheckout = document.querySelector('.price__result');
-const itemsCalc = document.querySelectorAll('.basket__content .price__calc');
 let count = 0;
 itemsCalc.forEach(e => {
     count += parseInt(e.innerText);
@@ -32,8 +44,6 @@ itemsCalc.forEach(e => {
 if (priceCheckout) {
     priceCheckout.innerText = count.toString();
 }
-var itemsPrice = document.querySelectorAll('.basket .price__calc');
-
 itemsPrice.forEach(e => {
     e.innerText += ' zł';
 });
